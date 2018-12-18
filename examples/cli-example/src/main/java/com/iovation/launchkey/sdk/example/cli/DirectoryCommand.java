5f54d01f69190d5052add232e3b5c6f5f7a8f4fc
Adam Englander <adam.englander@iovation.com>
2018-12-17 18:38:02 -0800

    Migrate to pico CLI to make it less painful to add options to auth requests

---
+++ examples/cli-example/src/main/java/com/iovation/launchkey/sdk/example/cli/DirectoryCommand.java
@@ -0,0 +1,90 @@
+package com.iovation.launchkey.sdk.example.cli;
+
+import com.iovation.launchkey.sdk.client.DirectoryClient;
+import com.iovation.launchkey.sdk.domain.directory.Device;
+import com.iovation.launchkey.sdk.domain.directory.DirectoryUserDeviceLinkData;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+@CommandLine.Command(name = "directory")
+public class DirectoryCommand {
+
+    @CommandLine.ParentCommand
+    LaunchKeyCommand launchKeyCommand;
+
+    @CommandLine.Parameters(index = "0", paramLabel = "<DIR_ID>",
+            description = "Directory ID. It is found in the Keys section of the Directory's page in Dashboard.")
+    String directoryId;
+
+    @CommandLine.Parameters(index = "1", paramLabel = "<KEY_FILE>",
+            description = "File location of the RSA Private Key of the RSA public/private key pair whose public key " +
+                    "is associated with the Directory.")
+    File privateKeyFile;
+
+    @CommandLine.Command(name = "device-link")
+    void deviceLink(@CommandLine.Parameters(paramLabel = "<UNIQUE_IDENTIFIER>",
+            description = "Unique identifier of the user for your application") String identifier) throws Exception {
+        DirectoryUserDeviceLinkData result = getDirectoryClient().linkDevice(identifier);
+        System.out.println();
+        System.out.println("Device link request successful");
+        System.out.println("    QR Code URL: " + result.getQrCodeUrl());
+        System.out.println("    Manual verification code: " + result.getCode());
+        System.out.println();
+    }
+
+    @CommandLine.Command(name = "device-unlink")
+    void deivceUnlink(
+            @CommandLine.Parameters(paramLabel = "<UNIQUE_IDENTIFIER>",
+                    description = "Unique identifier of the user for your application") String identifier,
+            @CommandLine.Parameters(paramLabel = "<DEVICE_ID>",
+                    description = "Identifier of the Device you wish to unink") String deviceId) throws Exception {
+        System.out.println();
+        getDirectoryClient().unlinkDevice(identifier, deviceId);
+        System.out.println("Device unlinked");
+        System.out.println();
+    }
+
+    @CommandLine.Command(name = "devices-list")
+    void devicesList(@CommandLine.Parameters(paramLabel = "<UNIQUE_IDENTIFIER>",
+            description = "Unique identifier of the user for your application") String identifier) throws Exception {
+        List<Device> devices = getDirectoryClient().getLinkedDevices(identifier);
+        System.out.println();
+        System.out.println("Devices:");
+        for (Device device : devices) {
+            System.out.println("  " + device.getId() + ":");
+            System.out.println("    Name:    " + device.getName());
+            System.out.println("    Type:    " + device.getType());
+            System.out.println("    Status:  " + device.getStatus());
+            System.out.println("    Created: " + device.getCreated());
+            System.out.println("    Updated: " + device.getUpdated());
+        }
+        System.out.println();
+    }
+
+    private DirectoryClient getDirectoryClient() throws Exception {
+        Scanner sc = new Scanner(privateKeyFile);
+        StringBuilder buffer = new StringBuilder();
+        while (sc.hasNextLine()) {
+            buffer.append(sc.nextLine()).append("\n");
+        }
+        String key = buffer.toString();
+
+        return launchKeyCommand.getFactoryFactory()
+                .makeDirectoryFactory(directoryId, key).makeDirectoryClient();
+    }
+
+    private static String naForNull(String value) {
+        return value == null ? "N/A" : value;
+    }
+
+    private static String naForNull(Enum value) {
+        return naForNull(value.name());
+    }
+
+    private static String naForNull(Boolean value) {
+        return naForNull(value.toString());
+    }
+}
