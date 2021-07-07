77093c767a7d185f230b3b2e368767c525b152d7
Cody Moncur <cmoncur@gmail.com>
2021-07-07 08:50:51 -0700

    Comment update

--- sdk/src/test/java/com/iovation/launchkey/sdk/transport/apachehttp/ApiRequestBuilderTest.java
+++ sdk/src/test/java/com/iovation/launchkey/sdk/transport/apachehttp/ApiRequestBuilderTest.java
@@ -18,7 +18,7 @@ public class ApiRequestBuilderTest extends TestCase {
         this.apiRequestBuilder = new ApiRequestBuilder(null,null,null, null, null, null, null, null, null);
     }
 
-    //NOTE: This test has an assertion utilizing a null because one cannot get the value unless it is inside the built JAR.
+    //NOTE: This test has an assertion utilizing a null because one cannot get the value unless it is inside the built
     @Test
     public void testBuildAddsProperUserAgentHeader() throws MarshallingError, CryptographyError {
         HttpUriRequest request = this.apiRequestBuilder.build(null);
